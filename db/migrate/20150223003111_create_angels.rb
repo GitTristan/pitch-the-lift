04a847c4a213988260b50d1933a883c16ad17253
Tristan Rubadeau <tristan@rubadeau.com>
2015-02-22 17:38:45 -0700

    angel table

--- db/migrate/20150223003111_create_angels.rb
+++ db/migrate/20150223003111_create_angels.rb
@@ -1,6 +1,10 @@
 class CreateAngels < ActiveRecord::Migration
   def change
     create_table :angels do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.boolean :accredited 
 
       t.timestamps null: false
     end
